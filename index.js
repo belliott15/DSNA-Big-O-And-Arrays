3851bce17aa1f759828c1606d5485cd4a9c2497e
Beau Elliott <belliott1516@gmail.com>
2022-07-11 09:39:08 -0700

    added push function to dsna test

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function push(arr, item) {}
+function push(arr, item) {
+    //loop through array to reach end item
+    const end = arr.length - 1;
+    for (let i = 0; i < arr.length; i++ ) {
+        //if there is no next item add item to end of array
+        if(arr.length === 0) return null 
+        arr[end + 1] = item;
+        return arr.length
+    }
+    
+}
 
 module.exports = { push };
